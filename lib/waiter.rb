9a3a647ce71585bdd559b562f0784fc539feadf1
Clint Richards <richardsclint17@gmail.com>
2021-01-07 01:02:27 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -6,10 +6,11 @@ class Waiter
   def initialize (name, age)
     @name = name
     @age = age
+    @all >> self
   end
 
   def self.all
-    @@all >> self
+    @@all
   end
 
   def new_meal(customer, total, tip=0)
